d77d2ca65c1e240d26937c9cba691bf872b491f6
unknown <gritsunov85@gmail.com>
2022-08-09 23:36:36 +0600

    DZ4

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number_one = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while(i < number_one){
+    if(i % 2 == 0) Console.WriteLine(i);
+    i++;
+}
